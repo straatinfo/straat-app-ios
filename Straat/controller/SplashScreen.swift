bf22999abe243ce62282fb859d96bf43f3a078ba
ralfrotamola <ralf.rotamla4@gmail.com>
2019-02-01 18:40:39 +0800

    added splashscreen and sb reference

--- Straat/controller/SplashScreen.swift
+++ Straat/controller/SplashScreen.swift
@@ -12,8 +12,16 @@ class SplashScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        interval()
     }
 
 
+    func interval() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+            
+            self.performSegue(withIdentifier: "nextVC", sender: nil)
+            
+        }
+    }
 }
 

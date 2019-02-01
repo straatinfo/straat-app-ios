3050bbd9aca69869073f5337207ad883fb2d6f01
ralfrotamola <ralf.rotamla4@gmail.com>
2019-02-01 16:22:10 +0800

    Added main view with sidebar menu

---
+++ Straat/controller/Main.swift
@@ -0,0 +1,55 @@
+//
+//  Main.swift
+//  Straat
+//
+//  Created by Global Array on 01/02/2019.
+//
+
+import UIKit
+
+class Main: UIViewController {
+
+    @IBOutlet weak var menu: UIBarButtonItem!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createMenu()
+        navColor()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    func createMenu() {
+        if revealViewController() != nil {
+            
+            menu.target = revealViewController()
+            menu.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            revealViewController().rightViewRevealWidth = 275
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+        } else {
+            print("problem in SWRVC")
+        }
+    }
+    
+    func navColor() {
+        navigationController?.navigationBar.tintColor = UIColor.white
+        navigationController?.navigationBar.barTintColor = UIColor.gray
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
+
+    
+    
+}

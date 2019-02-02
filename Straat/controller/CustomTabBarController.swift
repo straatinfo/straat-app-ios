8e275a6be4222b895f86a241b41e3617c3ced850
ralfrotamola <ralf.rotamla4@gmail.com>
2019-02-03 04:54:31 +0800

    deleted base view controller

--- Straat/controller/CustomTabBarController.swift
+++ Straat/controller/CustomTabBarController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MsgMainTabBarController: UITabBarController {
+class CustomTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
